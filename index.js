cb34d933e5458c5ae39198a366c84d0e3b48ffb4
Ralf <ralf@admins-MacBook-Air.local>
2023-09-09 15:03:16 +0200

    Handle OUT requests

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) {
         res.status(404).send(`Course ${req.params.id} not found`)
+        return;
     }
     res.send(course);
 });
@@ -46,6 +47,32 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send(`Course ${req.params.id} not found`)
+        return;
+    }
+
+    const {error} = validateCourse(req.body);
+    if (error)  {
+        const concatenatedMessages = error.details.map(item => item.message).join(' ');
+        res.status(400).send(concatenatedMessages);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    const { error, value } = schema.validate(course);
+    return { error, value };
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}.`));
