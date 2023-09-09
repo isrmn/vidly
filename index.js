cee9914182ee882c283b4d6924020d88de5ddb7c
Ralf <ralf@admins-MacBook-Air.local>
2023-09-09 15:30:01 +0200

    Handle DELETE request

--- index.js
+++ index.js
@@ -61,6 +61,19 @@ app.put('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
+app.delete('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send(`Course ${req.params.id} not found`)
+        return;
+    }
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
 function validateCourse(course) {
     const schema = Joi.object({
         name: Joi.string().min(3).required()
